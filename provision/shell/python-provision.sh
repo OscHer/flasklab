39d21ad664552e16b08e64b422fcf13c0de78bc0
Oscar <oscarherediallorens@gmail.com>
2024-11-07 16:21:58 +0100

    Add python provision script

---
+++ provision/shell/python-provision.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+######################################################################
+# @author      : Óscar Heredia (65867332+OscHer@users.noreply.github.com)
+# @file        : python-provision.sh
+# @created     : jueves nov 07, 2024 16:18:41 CET
+#
+# @description : python dependencies installation 
+######################################################################
+
+apt-get install python3-pip
+
+
+
